52cf3994857ee149741b2007a80b91823699994f
“Jen <Jen.j.eyring@gmail.com>
2022-06-08 13:05:40 -0500

    go

--- prepare.py
+++ prepare.py
@@ -54,3 +54,37 @@ def scale_data(train,
 #to use: 
     # type "scaler, train_scaled, validate_scaled, test_scaled = scale_data(train, validate, test, return_scaler=True)"
     #into your notebook. then type 'train_scaled' or 'validate_scaled' or 'test_scaled' to view copy of scaled df
+
+#creating another version of the scaler, as after evaluating, I need another split:
+def scale_data2(train,
+              validate,
+              test,
+              columns_to_scale=['bedroomcnt', 'bathroomcnt', 'taxvaluedollarcnt', 'calculatedfinishedsquarefeet'],
+              return_scaler=False):
+    """
+    Scales the 3 data splits.
+    Takes in train, validate and test data splits and then returns their scaled counterparts.
+    If return_scaler is True, the scaler object will be returned as well
+    """
+    train_scaled2 = train.copy()
+    validate_scaled2 = validate.copy()
+    test_scaled2 = test.copy()
+    
+    #using MinMaxScaler (best showing distribution once scaled)
+    scaler = MinMaxScaler()
+    scaler.fit(train[columns_to_scale])
+    
+    #creating a df that puts MinMaxScaler to work on the wanted columns and returns the split datasets and counterparts
+    train_scaled2[columns_to_scale] = pd.DataFrame(scaler.transform(train[columns_to_scale]),
+                                                 columns=train[columns_to_scale].columns.values).set_index([train.index.values])
+    
+    validate_scaled2[columns_to_scale] = pd.DataFrame(scaler.transform(validate[columns_to_scale]),
+                                                 columns=validate[columns_to_scale].columns.values).set_index([validate.index.values])
+    
+    test_scaled2[columns_to_scale] = pd.DataFrame(scaler.transform(test[columns_to_scale]),
+                                                 columns=test[columns_to_scale].columns.values).set_index([test.index.values])
+    
+    if return_scaler:
+        return scaler, train_scaled2, validate_scaled2, test_scaled2
+    else:
+        return train_scaled2, validate_scaled2, test_scaled2
